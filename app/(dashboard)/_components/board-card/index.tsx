d40fb4791e12ff617b557c982913c41079aa07fa
James <jameshowie17@outlook.com>
2024-02-07 13:39:10 +0000

    board card footer component. new board button.

--- app/(dashboard)/_components/board-card/index.tsx
+++ app/(dashboard)/_components/board-card/index.tsx
@@ -2,7 +2,10 @@
 
 import Image from "next/image";
 import Link from "next/link";
+import { formatDistanceToNow } from "date-fns";
 import { Overlay } from "./overlay";
+import { useAuth } from "@clerk/nextjs";
+import { Footer } from "./footer";
 
 interface BoardCardProps {
   id: string;
@@ -25,6 +28,12 @@ export const BoardCard = ({
   orgId,
   isFavourite,
 }: BoardCardProps) => {
+  const { userId } = useAuth();
+  const authorLabel = userId === authorId ? "You " : authorName;
+  const createdAtLabel = formatDistanceToNow(createdAt, {
+    addSuffix: true,
+  });
+
   return (
     <Link href={`/board/${id}`}>
       <div className="group aspect-[100/127] border rounded-lg flex flex-col justify-between overflow-hidden">
@@ -32,6 +41,14 @@ export const BoardCard = ({
           <Image src={imageUrl} alt={title} fill className="object-fit" />
           <Overlay />
         </div>
+        <Footer
+          isFavourite={isFavourite}
+          title={title}
+          authorLabel={authorLabel}
+          createdAtLabel={createdAtLabel}
+          onClick={() => {}}
+          disabled={false}
+        />
       </div>
     </Link>
   );
--- app/(dashboard)/_components/board-list.tsx
+++ app/(dashboard)/_components/board-list.tsx
@@ -6,6 +6,7 @@ import { EmptyBoards } from "./empty-boards";
 import { EmptyFavourites } from "./empty-favourites";
 import { EmptySearch } from "./empty-search";
 import { BoardCard } from "./board-card";
+import { NewBoardButton } from "./new-board-button";
 
 interface BoardListProps {
   orgId: string;
@@ -40,6 +41,7 @@ export const BoardList = ({ orgId, query }: BoardListProps) => {
         {query.favourites ? "Favourite boards" : "Team boards "}
       </h2>
       <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-4 lg:grid-cols-4 xl:grid-cols-5 2xl:grid-cols-6 gap-5 mt-8 pb-10">
+        <NewBoardButton orgId={orgId} />
         {data?.map((board) => (
           <BoardCard
             key={board._id}
