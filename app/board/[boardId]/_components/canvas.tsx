eb7ff2d3ecfd2f3f9dcb7fec1765e51e22ac3feb
James <jameshowie17@outlook.com>
2024-02-13 19:50:10 +0000

    canvas.ts types setup and toolbar undo, redo and select ToolButton now takes in states from canvas.tsx from liveblocks to show if the Icon can   undo or redo depending on if the user has done anything that can be undone or redone, select state is automatically enabled when nothing has been done

--- app/board/[boardId]/_components/canvas.tsx
+++ app/board/[boardId]/_components/canvas.tsx
@@ -1,16 +1,28 @@
 "use client";
 
+import { useState } from "react";
+
+import { CanvasMode, CanvasState } from "@/types/canvas";
+
 import { Info } from "./info";
 import { Participants } from "./participants";
 import { Toolbar } from "./toolbar";
+import { useCanRedo, useCanUndo, useHistory } from "@/liveblocks.config";
 
-import { useSelf } from "@/liveblocks.config";
+// import { useSelf } from "@/liveblocks.config";
 
 interface CanvasProps {
   boardId: string;
 }
 
 export const Canvas = ({ boardId }: CanvasProps) => {
+  const [canvasState, setCanvasState] = useState<CanvasState>({
+    mode: CanvasMode.None,
+  });
+
+  const history = useHistory();
+  const canRedo = useCanRedo();
+  const canUndo = useCanUndo();
   // const info = useSelf((me) => me.info);
   // console.log(info)
 
@@ -18,7 +30,14 @@ export const Canvas = ({ boardId }: CanvasProps) => {
     <main className="h-full w-full relative bg-neutral-100 touch-none">
       <Info boardId={boardId} />
       <Participants />
-      <Toolbar />
+      <Toolbar
+        canvasState={canvasState}
+        setCanvasState={setCanvasState}
+        canRedo={canRedo}
+        canUndo={canUndo}
+        redo={history.redo}
+        undo={history.undo}
+      />
     </main>
   );
 };
--- app/board/[boardId]/_components/toolbar.tsx
+++ app/board/[boardId]/_components/toolbar.tsx
@@ -8,17 +8,36 @@ import {
   Type,
   Undo2,
 } from "lucide-react";
+
 import { ToolButton } from "./tool-button";
 
-export const Toolbar = () => {
+import { CanvasMode, CanvasState } from "@/types/canvas";
+
+interface ToolbarProps {
+  canvasState: CanvasState;
+  setCanvasState: (newState: CanvasState) => void;
+  undo: () => void;
+  redo: () => void;
+  canUndo: boolean;
+  canRedo: boolean;
+}
+
+export const Toolbar = ({
+  canvasState,
+  setCanvasState,
+  undo,
+  redo,
+  canUndo,
+  canRedo,
+}: ToolbarProps) => {
   return (
     <div className="absolute top-[50%] -translate-y-[50%] left-2 flex flex-col gap-y-4">
       <div className="bg-white rounded-md p-1.5 flex gap-y-1 flex-col items-center shadow-md">
         <ToolButton
           label="Select"
           icon={MousePointer2}
-          onClick={() => {}}
-          isActive={false}
+          onClick={() => setCanvasState({ mode: CanvasMode.None })}
+          isActive={canvasState.mode === CanvasMode.None}
         />
         <ToolButton
           label="Text"
@@ -55,14 +74,14 @@ export const Toolbar = () => {
         <ToolButton
           label="Undo"
           icon={Undo2}
-          onClick={() => {}}
-          isDisabled={true}
+          onClick={undo}
+          isDisabled={!canUndo}
         />
         <ToolButton
           label="Redo"
           icon={Redo2}
-          onClick={() => {}}
-          isDisabled={true}
+          onClick={redo}
+          isDisabled={!canRedo}
         />
       </div>
     </div>
